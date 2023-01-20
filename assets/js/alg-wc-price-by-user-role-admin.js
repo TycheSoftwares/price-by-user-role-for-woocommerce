0a99046e08771eff227a24b46359264870e4313f
nidhinikulpatel <tatosaniyanidhi@gmail.com>
2023-01-20 14:33:36 +0530

    Fix -#21
    fixed phpcs errors.

--- assets/js/alg-wc-price-by-user-role-admin.js
+++ assets/js/alg-wc-price-by-user-role-admin.js
@@ -1,10 +1,9 @@
 jQuery( document ).ready(
     function($) {
-	    var pbur_checkbox_for_order_role = $( '#checkbox_pbur' ).val();
-    
+        var pbur_checkbox_for_order_role = $( '#checkbox_pbur' ).val();
         $( '#alg_wc_pbur_select_role' ).change(
             function() {
-                var order_id = pbur_order_id.order_id;
+                var order_id                 = pbur_order_id.order_id;
                 pbur_checkbox_for_order_role = jQuery( '#alg_wc_pbur_select_role' ).val();
                 if ( $( '#checkbox_pbur' ).is( ":checked" ) ) {
                     var pbur_check  = true;
@@ -15,8 +14,13 @@ jQuery( document ).ready(
                     pbur_check: pbur_check,
                     action: 'alg_wc_pbur_order_role'
                 };
-                jQuery.post( pbur_order_role.ajax_url, data, function() {
-                });
+                jQuery.post(
+                    pbur_order_role.ajax_url,
+                    data,
+                    function() {
+                        console.log( data );
+                    }
+                );
             }
         );
     }
